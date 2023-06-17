a3356c20f9e6c201d45bd8cf29625710403d15ee
albertlin <albertlin@nine-yi.com>
2023-06-17 20:04:40 +0800

    [var 6] done

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,8 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+// let 可以不賦予數值，但是const必須賦予
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
